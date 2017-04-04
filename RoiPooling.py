97450e95504bd2b1035bce79d6a0d0afea94129d
Frederic <frederic.branchaud-charron@usherbrooke.ca>
2017-04-04 11:26:10 -0400

    Add batch support for TF

--- RoiPooling.py
+++ RoiPooling.py
@@ -1,5 +1,7 @@
-from keras.engine.topology import Layer
 import keras.backend as K
+import tensorflow as tf
+from keras.engine.topology import Layer
+
 
 class RoiPooling(Layer):
     '''ROI pooling layer for 2D inputs.
@@ -14,15 +16,16 @@ class RoiPooling(Layer):
     # Input shape
         list of two 4D tensors [X_img,X_roi] with shape:
         X_img:
-        `(1, channels, rows, cols)` if dim_ordering='th'
+        `(batch_size, channels, rows, cols)` if dim_ordering='th'
         or 4D tensor with shape:
-        `(1, rows, cols, channels)` if dim_ordering='tf'.
+        `(batch_size, rows, cols, channels)` if dim_ordering='tf'.
         X_roi:
-        `(1,num_rois,4)` list of rois, with ordering (x,y,w,h)
+        `(batch_size,num_rois,4)` list of rois, with ordering (x,y,w,h)
     # Output shape
         3D tensor with shape:
-        `(1, num_rois, channels * sum([i * i for i in pool_list])`
+        `(batch_size, num_rois, channels * sum([i * i for i in pool_list])`
     '''
+
     def __init__(self, pool_list, num_rois, **kwargs):
 
         self.dim_ordering = K.image_dim_ordering()
@@ -45,16 +48,16 @@ class RoiPooling(Layer):
         return (None, self.num_rois, self.nb_channels * self.num_outputs_per_channel)
 
     def get_config(self):
-        config = {'pool_list': self.pool_list,'num_rois':self.num_rois}
+        config = {'pool_list': self.pool_list, 'num_rois': self.num_rois}
         base_config = super(RoiPooling, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
-
     def call(self, x, mask=None):
 
-        assert(len(x) == 2)
+        assert (len(x) == 2)
 
         img = x[0]
+        print(img)
         rois = x[1]
 
         input_shape = K.shape(img)
@@ -63,57 +66,43 @@ class RoiPooling(Layer):
 
         for roi_idx in range(self.num_rois):
 
-            x = rois[0,roi_idx,0]
-            y = rois[0,roi_idx,1]
-            w = rois[0,roi_idx,2]
-            h = rois[0,roi_idx,3]
+            x = K.reshape(rois[:, roi_idx, 0], [-1, 1])
+            y = K.reshape(rois[:, roi_idx, 1], [-1, 1])
+            w = K.reshape(rois[:, roi_idx, 2], [-1, 1])
+            h = K.reshape(rois[:, roi_idx, 3], [-1, 1])
 
-            row_length = [w / i for i in self.pool_list]
-            col_length = [h / i for i in self.pool_list]
+            row_length = K.reshape(K.stack([w / i for i in self.pool_list], 1), [-1, len(self.pool_list)])
+            col_length = K.reshape(K.stack([h / i for i in self.pool_list], 1), [-1, len(self.pool_list)])
 
-            if self.dim_ordering == 'th':
+            def fun(elem):
+                x_crop_in, x_in, y_in, row_in, col_in = elem
+                acc = []
                 for pool_num, num_pool_regions in enumerate(self.pool_list):
                     for ix in range(num_pool_regions):
                         for jy in range(num_pool_regions):
-                            x1 = x + ix * col_length[pool_num]
-                            x2 = x1 + col_length[pool_num]
-                            y1 = y + jy * row_length[pool_num]
-                            y2 = y1 + row_length[pool_num]
+                            x1 = x_in + ix * col_in[pool_num]
+                            x2 = x1 + col_in[pool_num]
+                            y1 = y_in + jy * row_in[pool_num]
+                            y2 = y1 + row_in[pool_num]
 
-                            x1 = K.cast(K.round(x1), 'int32')
-                            x2 = K.cast(K.round(x2), 'int32')
-                            y1 = K.cast(K.round(y1), 'int32')
-                            y2 = K.cast(K.round(y2), 'int32')
-                            
-                            new_shape = [input_shape[0], input_shape[1],
-                                         y2 - y1, x2 - x1]
-                            x_crop = img[:, :, y1:y2, x1:x2]
-                            xm = K.reshape(x_crop, new_shape)
-                            pooled_val = K.max(xm, axis=(2, 3))
-                            outputs.append(pooled_val)
+                            x1 = K.cast(K.round(x1), 'int32')[0]
+                            x2 = K.cast(K.round(x2), 'int32')[0]
+                            y1 = K.cast(K.round(y1), 'int32')[0]
+                            y2 = K.cast(K.round(y2), 'int32')[0]
 
-            elif self.dim_ordering == 'tf':
-                for pool_num, num_pool_regions in enumerate(self.pool_list):
-                    for ix in range(num_pool_regions):
-                        for jy in range(num_pool_regions):
-                            x1 = x + ix * col_length[pool_num]
-                            x2 = x1 + col_length[pool_num]
-                            y1 = y + jy * row_length[pool_num]
-                            y2 = y1 + row_length[pool_num]
-
-                            x1 = K.cast(K.round(x1), 'int32')
-                            x2 = K.cast(K.round(x2), 'int32')
-                            y1 = K.cast(K.round(y1), 'int32')
-                            y2 = K.cast(K.round(y2), 'int32')
-
-                            new_shape = [input_shape[0], y2 - y1,
+                            new_shape = [1, y2 - y1,
                                          x2 - x1, input_shape[3]]
-                            x_crop = img[:, y1:y2, x1:x2, :]
+
+                            x_crop = x_crop_in[y1:y2, x1:x2, :]
                             xm = K.reshape(x_crop, new_shape)
                             pooled_val = K.max(xm, axis=(1, 2))
-                            outputs.append(pooled_val)
+                            acc.append(pooled_val)
+                return acc
 
-        final_output = K.concatenate(outputs,axis = 0)
-        final_output = K.reshape(final_output,(1,self.num_rois,self.nb_channels * self.num_outputs_per_channel))
+            x = tf.map_fn(fun, [img, x, y, row_length, col_length], dtype=[tf.float32] * (sum(self.pool_list) * 3))
+            outputs.extend(x)
+
+        final_output = K.concatenate(outputs, axis=1)
+        final_output = K.reshape(final_output, (-1, self.num_rois, self.nb_channels * self.num_outputs_per_channel))
 
         return final_output
--- test_roi_pooling.py
+++ test_roi_pooling.py
@@ -24,55 +24,59 @@ model = Model([in_img, in_roi], out_roi_pool)
 model.summary()
 
 model.compile(loss='mse', optimizer='sgd')
+batch_size = 2
 
 for img_size in [8, 16, 32]:
 
     if dim_ordering == 'th':
-        X_img = np.random.rand(1, num_channels, img_size, img_size)
+        X_img = np.random.rand(batch_size, num_channels, img_size, img_size)
         row_length = [float(X_img.shape[2]) / i for i in pooling_regions]
         col_length = [float(X_img.shape[3]) / i for i in pooling_regions]
     elif dim_ordering == 'tf':
-        X_img = np.random.rand(1, img_size, img_size, num_channels)
+        X_img = np.random.rand(batch_size, img_size, img_size, num_channels)
         row_length = [float(X_img.shape[1]) / i for i in pooling_regions]
         col_length = [float(X_img.shape[2]) / i for i in pooling_regions]
 
-    X_roi = np.array([[0, 0, img_size / 1, img_size / 1],
-                      [0, 0, img_size / 2, img_size / 2]])
+    X_roi = np.array([[0, 0, img_size // 1, img_size // 1],
+                      [0, 0, img_size // 2, img_size // 2]],dtype=np.int32)
 
-    X_roi = np.reshape(X_roi, (1, num_rois, 4))
+    X_roi = np.tile(X_roi,(batch_size,1))
+
+    X_roi = np.reshape(X_roi, (batch_size, num_rois, 4))
 
     Y = model.predict([X_img, X_roi])
+    for batch in range(batch_size):
+        for roi in range(num_rois):
 
-    for roi in range(num_rois):
+            if dim_ordering == 'th':
+                X_curr = X_img[batch, :, X_roi[batch, roi, 0]:X_roi[batch, roi, 2], X_roi[batch, roi, 1]:X_roi[batch, roi, 3]]
+                row_length = [float(X_curr.shape[1]) / i for i in pooling_regions]
+                col_length = [float(X_curr.shape[2]) / i for i in pooling_regions]
+            elif dim_ordering == 'tf':
+                X_curr = X_img[batch, X_roi[batch, roi, 0]:X_roi[batch, roi, 2], X_roi[batch, roi, 1]:X_roi[batch, roi, 3], :]
+                row_length = [float(X_curr.shape[0]) / i for i in pooling_regions]
+                col_length = [float(X_curr.shape[1]) / i for i in pooling_regions]
 
-        if dim_ordering == 'th':
-            X_curr = X_img[0, :, X_roi[0, roi, 0]:X_roi[0, roi, 2], X_roi[0, roi, 1]:X_roi[0, roi, 3]]
-            row_length = [float(X_curr.shape[1]) / i for i in pooling_regions]
-            col_length = [float(X_curr.shape[2]) / i for i in pooling_regions]
-        elif dim_ordering == 'tf':
-            X_curr = X_img[0, X_roi[0, roi, 0]:X_roi[0, roi, 2], X_roi[0, roi, 1]:X_roi[0, roi, 3], :]
-            row_length = [float(X_curr.shape[0]) / i for i in pooling_regions]
-            col_length = [float(X_curr.shape[1]) / i for i in pooling_regions]
+            idx = 0
 
-        idx = 0
+            for pool_num, num_pool_regions in enumerate(pooling_regions):
+                print("POOL NUM:",pool_num)
+                for ix in range(num_pool_regions):
+                    for jy in range(num_pool_regions):
+                        for cn in range(num_channels):
 
-        for pool_num, num_pool_regions in enumerate(pooling_regions):
-            for ix in range(num_pool_regions):
-                for jy in range(num_pool_regions):
-                    for cn in range(num_channels):
+                            x1 = int(round(ix * col_length[pool_num]))
+                            x2 = int(round(ix * col_length[pool_num] + col_length[pool_num]))
+                            y1 = int(round(jy * row_length[pool_num]))
+                            y2 = int(round(jy * row_length[pool_num] + row_length[pool_num]))
 
-                        x1 = int(round(ix * col_length[pool_num]))
-                        x2 = int(round(ix * col_length[pool_num] + col_length[pool_num]))
-                        y1 = int(round(jy * row_length[pool_num]))
-                        y2 = int(round(jy * row_length[pool_num] + row_length[pool_num]))
+                            if dim_ordering == 'th':
+                                m_val = np.max(X_curr[cn, y1:y2, x1:x2])
+                            elif dim_ordering == 'tf':
+                                m_val = np.max(X_curr[y1:y2, x1:x2, cn])
 
-                        if dim_ordering == 'th':
-                            m_val = np.max(X_curr[cn, y1:y2, x1:x2])
-                        elif dim_ordering == 'tf':
-                            m_val = np.max(X_curr[y1:y2, x1:x2, cn])
-
-                        np.testing.assert_almost_equal(
-                            m_val, Y[0, roi, idx], decimal=6)
-                        idx += 1
+                            np.testing.assert_almost_equal(
+                                m_val, Y[batch, roi, idx], decimal=6)
+                            idx += 1
                         
 print('Passed roi pooling test')
